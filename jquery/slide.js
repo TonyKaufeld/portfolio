bded848e6a8757769fef31e803c1ee4e62bc19e3
Tony <tony1999k@live.nl>
2018-06-03 19:53:20 +0200

    update

--- jquery/slide.js
+++ jquery/slide.js
@@ -1,6 +1,6 @@
 //-- navigation buttons ------------------------- --/
 
-$('.position1').click(function () {
+/*$('.position1').click(function () {
     $('html, body').stop().animate({
         scrollTop: $("#home").offset().top
     }, 2000);
@@ -28,7 +28,7 @@ $('.position5').click(function () {
     $('html, body').stop().animate({
         scrollTop: $("#contact").offset().top - 500
     }, 2000);
-});
+});*/
 
 //-- overmij fadeIn effect --------------------- --/
 
@@ -105,6 +105,62 @@ $.scrollify({
     afterRender:function() {}
 });
 
-//-- cc-typing ---------------------------- --/
+//-- cc-typing ----------------------------- --/
 
 $(".css-typing").delay(1000).fadeIn(500);
+
+//-- li hover effect ----------------------- --/
+
+$('li').hover(
+    function(){ $(this).addClass('hover') },
+    function(){ $(this).removeClass('hover') }
+);
+
+// -- active menu --------------------------- --/
+
+$('#welkom').waypoint(function() {
+    $("li").removeClass('actief');
+    $( "li:nth-child(1)" ).addClass( "actief" );
+});
+
+$('#over_mij').waypoint(function() {
+    $("li").removeClass('actief');
+    $( "li:nth-child(2)" ).addClass( "actief" );
+});
+
+$('#opleiding').waypoint(function() {
+    $("li").removeClass('actief');
+    $( "li:nth-child(3)" ).addClass( "actief" );
+});
+
+$('#project').waypoint(function() {
+    $("li").removeClass('actief');
+    $( "li:nth-child(4)" ).addClass( "actief" );
+});
+
+$('#contact').waypoint(function() {
+    $("li").removeClass('actief');
+    $( "li:nth-child(5)" ).addClass( "actief" );
+});
+
+//-- add smooth scrolling ------------------- --/
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+
