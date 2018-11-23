8653f3034d98a36c3588ca75e635df64cbcc369a
Tony Kaufeld <tony1999k@live.nl>
2018-11-23 20:21:32 +0100

    update

---
+++ Website/index.php
@@ -0,0 +1,28 @@
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="beschrijving">
+    <meta name="author" content="Auteur">
+    <title></title>
+    <link rel="stylesheet" href="Css/header.css">
+    <script src="Javascript/login.js"></script>
+    
+</head>
+
+<body>
+
+    <div id="login-form">
+        <div id="login-container">
+
+            <input type="text" placeholder="Enter Username" id="username" required><br/>
+            <input type="password" placeholder="Enter Password" id="password" required><br/>
+            
+            <button onclick="myFunction()">Login</button>
+
+            <p id="username_value"></p>
+            <p id="password_value"></p>
+        </div>
+    </div>
+
+</body>
